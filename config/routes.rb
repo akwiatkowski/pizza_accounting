d9d618bb3393d52e1b8644a8c8371a8a868ef76e
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-06-22 15:54:16 +0200

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 PizzaAccounting::Application.routes.draw do
+
+  resources :users
   devise_for :users
 
   root :to => "home#index"
+  get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
